9b2c5236fa6073779311e6cbbd5f5b3c509c7983
이진섭 <leejinsup@gmail.com>
2017-03-27 11:08:30 +0900

    기레기 이름 뽑아내기

--- crawler5.py
+++ crawler5.py
@@ -1,25 +1,25 @@
 #-*- coding: utf-8 -*-
 # 뷰티플수프를 연습 파일 2
 # 범위내 네이버 뉴스 파싱
+# 기레기 이름 뽑아내기
 
 import re
 import urllib
 from bs4 import BeautifulSoup
 
-boardurl = "http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009136"
+boardurl = "http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009133400"
 
-for i in range(801, 802, 1):
-    stri = str(i)
-    html = urllib.urlopen(boardurl + stri)
-    soup = BeautifulSoup(html, "lxml")
+html = urllib.urlopen(boardurl)
+soup = BeautifulSoup(html, "lxml")
 
-    article_time = str(soup.find_all('span', {'class':'t11'}))
-    article_title = soup.title
+textsoup = str(soup)
 
-    timep = re.compile('\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}')
-    authorp = re.compile('[기][자]')
+timep = re.compile('\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}')
+article_time = timep.findall(textsoup)
 
-    article_time = timep.findall(article_time)
-    author = authorp.findall(soup.string)
+print article_time
 
-    print article_time,str(i),article_title
+regex = '\D\D\D\D\D\D\D\D\D\D기자'
+result = re.findall(regex, textsoup)
+print type(result)
+print("\n".join(result))
