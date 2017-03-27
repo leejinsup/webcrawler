73a57c92057be3f1e7e496e95b64da19d7554e05
이진섭 <leejinsup@gmail.com>
2017-03-27 11:26:32 +0900

    기레기 주소뽑기도 성공 ㅋ

---
+++ crawler6.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+# 뷰티플수프를 연습 파일 2
+# 범위내 네이버 뉴스 파싱
+# 기레기 이메일 뽑아내기
+
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+boardurl = "http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009133401"
+
+html = urllib.urlopen(boardurl)
+soup = BeautifulSoup(html, "lxml")
+
+textsoup = str(soup)
+
+timep = re.compile('\d{4}[-]\d{2}[-]\d{2}\s\d{2}[:]\d{2}')
+article_time = timep.findall(textsoup)
+
+print article_time
+
+regex = '[\S]+@[\w][^<]+'
+result = re.findall(regex, textsoup)
+print type(result)
+print("\n".join(result))
